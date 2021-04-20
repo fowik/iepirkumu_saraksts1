528ff1fcd6d82fb467f185b8b6dc8d7c712eea1e
fowik <ilarimsa937@gmail.com>
2021-04-20 16:07:50 +0300

    added new

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+document.getElementById('newPurchase').addEventListener('click', () => {
+    let popUp = document.getElementById('popUp');
+
+    popUp.style.display = 'block';
+})
+
+document.getElementsByClassName('addPurchase').addEventListener('click', () => {
+
+})
+
