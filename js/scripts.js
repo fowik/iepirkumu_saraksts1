92ec8adbc9fd5c5373b68b868c78c3d01123c731
fowik <ilarimsa937@gmail.com>
2021-04-24 23:22:05 +0300

    Saite darbojas, iepirkumu un daudzumu var ievadit

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,11 @@
 const POP_UP = document.getElementById('popUp');
+let iepirkumi = [];
+
+window.addEventListener('load', () => {
+    iepirkumi = JSON.parse(localStorage.getItem("iepirkumi") || "[]");
+    console.log(iepirkumi)
+    render();
+});
 
 document.getElementById('newPurchase').addEventListener('click', () => {
     POP_UP.style.display = 'block';
@@ -7,11 +14,30 @@ document.getElementById('newPurchase').addEventListener('click', () => {
 document.getElementById('addPurchase').addEventListener('click', () => {
     POP_UP.style.display = 'none';
 
-    let iepirkums = {purchase: purchase.value, daudzums: daudzums.value};
+    let purchases = {purchase: purchase.value, daudzums: daudzums.value};
      
-    purchase.value = ""
-    daudzums.value = ""
+    purchase.value = "";
+    daudzums.value = "";
 
-    console.log(iepirkums);
+    iepirkumi.push(purchases);
+
+    render();
 })
 
+function render() {
+    let iepirkumaList = document.getElementById('iepirkumaList');
+    iepirkumaList.innerHTML = "";
+
+    for(let i = 0; i < iepirkumi.length; i++) {
+        let iepirkumiS = `        
+        <div class="iepirkumiS">
+            <h3>Iepirkums: ${iepirkumi[i].purchase}</h3>
+            <h3>Daudzums: ${iepirkumi[i].daudzums}</h3>
+            <button id="deleteBtn">Dzēst</button>
+        </div>`;
+        iepirkumaList.innerHTML += iepirkumiS;
+    }
+
+    localStorage.setItem('iepirkumi', JSON.stringify(iepirkumi));
+
+}
